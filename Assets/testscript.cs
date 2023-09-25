a37a63c2db395dd52b74a6d587943e65349a2fe6
Daniil Maksimov <d.maksimov@xlab-game.com>
2023-09-26 01:15:42 +0300

    HW 80%

--- Assets/testscript.cs
+++ Assets/testscript.cs
@@ -1,29 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Test
-{ 
-
-}
-public class testscript : MonoBehaviour
+public class TestScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
-        short s = 0;
-        int a = 1;
-        long b = 3;
-        float y = 4;
-        double p = 5;
-        Debug.Log($"short : {short.MinValue} - {short.MaxValue}");
-        object o = a;
-        double d2 = (double)o;
-
-        a = (int)b;
-        Test mc = new Test();
-        TestFunc(mc);
+    { 
+        MyList myList = new MyList();
+        myList.Add(1);
+        myList.Add(5);
+        myList.Insert(1, 3);
+        myList.Capacity = 4;
+        myList.Remove(3);
+        myList.RemoveAt(0);
     }
 
     // Update is called once per frame
@@ -31,25 +21,116 @@ public class testscript : MonoBehaviour
     {
         
     }
-    public void TestFunc(Test mc)
-    {
-        i = 10;
-        Debug.Log(i);
-    
-    
-    }  
 }
 public class MyList
 {
-    public void Push(int i)
-    { 
-    }
-    public int Indexof(int item)
+    public int[] m_array = new int[4];
+    
+    public int Count { get; private set; }
+
+    public int Capacity { get { return 0; } set { } }
+
+    public MyList()
     {
-        return 0;
+        Count = 0;
+        Capacity = 0;
     }
-    public void Remove(int item) { }
-    public void Clear() { }
-    public void Insert(int index, int item) { }
-    public void RemoveAt(int index) { }
+
+    public MyList(int capacity)
+    {
+        Capacity = capacity;
+        Count = 0;
+    }
+
+    public int this[int index] { get { return m_array[index]; } set { m_array[index] = value; } }
+
+    public void Add(int item)
+    {
+        if (Count == 4)
+        {
+            return;
+        }
+        this[Count] = item;
+        Count++;
+    }
+
+    public void Insert(int index, int item)
+    {
+        if ((index < 0 || index > Count) && Count >= 4)
+        {
+            return;
+        }
+        for (int i = Count; i > index; i--)
+        {
+            this[i] = this[i - 1];
+        }
+        this[index] = item;
+        Count++;
+    }
+
+    public int IndexOf(int item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (this[i] == item)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public bool Remove(int item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (this[i] == item)
+            {
+                for (int j = i; j < Count - 1; j++)
+                {
+                    this[j] = this[j + 1];
+                }
+                this[Count - 1] = 0;
+                Count--;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void RemoveAt(int index)
+    {
+        if (index < 0 || index > Count)
+        {
+            return;
+        }
+        
+        for (int i = index; i < Count - 1; i++)
+        {
+            this[i] = this[i + 1];
+        }
+        this[Count - 1] = 0;
+        Count--;
+    }
+
+    public bool Contains(int item)
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            if (this[i] == item)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < Count; i++)
+        {
+            this[i] = 0;
+        } 
+        Count = 0;
+    }
 }
